049b20ef47e0067b5d47c5fae6cc7f837a28a8eb
Nimitha-jagadeesha <nimitha1jagadeesha@gmail.com>
2020-09-03 21:27:54 +0530

    FlatList

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet, Image } from "react-native";
-import { Card } from "react-native-paper";
+import { Text, View, StyleSheet, Image, FlatList } from "react-native";
+import { Card, FAB } from "react-native-paper";
 
 export class Home extends Component {
   render() {
@@ -21,17 +21,45 @@ export class Home extends Component {
         id: 3,
         name: "ramesh",
         position: "ML expert",
-        pic: "https://www.nationalparks.nsw.gov.au/-/media/npws/images/parks/blue-mountains-national-park/blackheath-area/pulpit-rock-track/pulpit-walking-track-01.jpg",
+        pic:
+          "https://www.nationalparks.nsw.gov.au/-/media/npws/images/parks/blue-mountains-national-park/blackheath-area/pulpit-rock-track/pulpit-walking-track-01.jpg",
       },
       {
         id: 4,
         name: "hitesh",
         position: "web dev",
-        pic: "https://tse4.mm.bing.net/th?id=OIP.HSwUQfniV1zAJo07eMF03wHaC4&pid=Api&P=0&w=397&h=155",
+        pic:
+          "https://tse4.mm.bing.net/th?id=OIP.HSwUQfniV1zAJo07eMF03wHaC4&pid=Api&P=0&w=397&h=155",
+      },
+      {
+        id: 5,
+        name: "mukesh",
+        position: "web dev",
+        pic: "https://i.ytimg.com/vi/f6eXSnajYOA/maxresdefault.jpg",
+      },
+      {
+        id: 6,
+        name: "suresh",
+        position: "android dev",
+        pic: "https://www.holidify.com/images/bgImages/PAHALGAM.jpg",
+      },
+      {
+        id: 7,
+        name: "ramesh",
+        position: "ML expert",
+        pic:
+          "https://www.nationalparks.nsw.gov.au/-/media/npws/images/parks/blue-mountains-national-park/blackheath-area/pulpit-rock-track/pulpit-walking-track-01.jpg",
+      },
+      {
+        id: 8,
+        name: "hitesh",
+        position: "web dev",
+        pic:
+          "https://tse4.mm.bing.net/th?id=OIP.HSwUQfniV1zAJo07eMF03wHaC4&pid=Api&P=0&w=397&h=155",
       },
     ];
 
-    const renderList = data.map((item) => {
+    const renderList = (item) => {
       return (
         <Card style={styles.card} key={item.id}>
           <View style={styles.cardView}>
@@ -48,8 +76,25 @@ export class Home extends Component {
           </View>
         </Card>
       );
-    });
-    return <View>{renderList}</View>;
+    };
+    return (
+      <View>
+        <FlatList
+          data={data}
+          renderItem={({ item }) => {
+            return renderList(item);
+          }}
+          keyExtractor={(item) => `${item.id}`}
+        />
+        <FAB
+          style={styles.fab}
+          small={false}
+          icon="plus"
+          theme={{colors:{accent:"#00a6ff"}}}
+          onPress={() => console.log("Pressed")}
+        />
+      </View>
+    );
   }
 }
 
@@ -64,5 +109,11 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 18,
+  },
+  fab: {
+    position: "absolute",
+    margin: 16,
+    right: 0,
+    bottom: 0,
   },
 });
